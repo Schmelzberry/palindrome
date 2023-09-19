2281f7c77cc08671c5f54caf086e3ab429448fdc
Jake Elsberry <jelsberry96@gmail.com>
2023-09-19 14:50:52 -0700

    UI added, we are kinda mean > > Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com> Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com> Co-authored-by: Joey Palchak <joey.palchak@gmail.com>

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,3 +1,4 @@
+using PalindromeChecker.Models;
 using System;
 
 namespace PalindromeChecker
@@ -6,7 +7,33 @@ namespace PalindromeChecker
   {
     static void Main()
     {
-      // C# Program code goes here.
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Palindrome checker! You guessed it, we check for palindromes.");
+      Console.WriteLine("Please enter a word you want to check:");
+      string newUser = Console.ReadLine();
+      Palindrome newPal = new Palindrome(newUser);
+      bool result = newPal.CheckArray();
+    //  Console.WriteLine(result);
+      if (result) 
+      {
+      Console.WriteLine("That's definitely a palindrome!");
+      }
+      else
+      {
+      Console.WriteLine("That's not a palindrome. What are you thinking, you idiot sandwich?");
+      }
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Do you want to try another possible palindrome?");
+      Console.WriteLine("Enter 'yes' to go again, otherwise enter any key to exit.");
+      string otherUser = Console.ReadLine();
+      if (otherUser.ToLower() == "yes")
+      {
+      Main();
+      }
+      else
+      {
+      Console.WriteLine("Later skater.");
+      }
     }
   }
 }
