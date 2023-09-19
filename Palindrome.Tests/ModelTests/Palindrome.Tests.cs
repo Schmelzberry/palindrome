6290f12d1f51df391caa21aa34ae356f359fca9b
Jake Elsberry <jelsberry96@gmail.com>
2023-09-19 14:14:55 -0700

    compare two arrays, introduce boolean > > Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com> Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com> Co-authored-by: Joey Palchak <joey.palchak@gmail.com>

--- Palindrome.Tests/ModelTests/Palindrome.Tests.cs
+++ Palindrome.Tests/ModelTests/Palindrome.Tests.cs
@@ -49,7 +49,53 @@ namespace PalindromeChecker.Tests
       char[] pals = userInput.ToCharArray();
       char[] result = newPal.UserArray();
       //assert
-      Assert.AreEqual(pals, result);
+      CollectionAssert.AreEqual(pals, result);
+    }
+
+    [TestMethod]
+    public void ReverseArray_ReversesUserArray_Array()
+    {
+      // arrange
+      char[] expectedArray = {'g','n','i','r','t','s'};
+      Palindrome newPal = new Palindrome("string");
+      // act
+      char[] result = newPal.ReverseArray();
+      // assert
+      CollectionAssert.AreEqual(expectedArray, result);
+    }
+    
+    [TestMethod]
+    public void ReverseArray_ReversesUserArrayWithUpper_Array()
+    {
+      // arrange
+      char[] expectedArray = {'g','n','i','r','t','s'};
+      Palindrome newPal = new Palindrome("String");
+      // act
+      char[] result = newPal.ReverseArray();
+      // assert
+      CollectionAssert.AreEqual(expectedArray, result);
+    }
+
+    [TestMethod]
+    public void CheckArray_CompareUserArrayWithReverseUserArrayTrue_Bool()
+    {
+      //arrange 
+      Palindrome newPal = new Palindrome("tacocat");
+      //act
+      bool arrayCheck = newPal.CheckArray();
+      //assert
+      Assert.AreEqual(arrayCheck, true);
+    }
+
+    [TestMethod]
+    public void CheckArray_CompareUserArrayWithReverseUserArrayFalse_Bool()
+    {
+      //arrange 
+      Palindrome newPal = new Palindrome("notreal");
+      //act
+      bool arrayCheck = newPal.CheckArray();
+      //assert
+      Assert.AreEqual(arrayCheck, false);
     }
   }
 }
--- Palindrome/Models/Palindrome.cs
+++ Palindrome/Models/Palindrome.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace PalindromeChecker.Models
 {
@@ -9,13 +10,37 @@ namespace PalindromeChecker.Models
 
     public Palindrome(string userInput)
     {
-      Pal = userInput;
+      Pal = userInput.ToLower();
+      
     }
 
+    // create and return an array containing each character of user's string
     public char[] UserArray()
     {
       char[] charArray = Pal.ToCharArray();
       return charArray;
+    }
+    // create and return an array containing each character of user's string, in reversed order
+    public char[] ReverseArray()
+    {
+      char[] charArray = this.UserArray();
+      Array.Reverse(charArray);
+      return charArray;
+    }
+
+    public bool CheckArray()
+    {
+      char[] userInputArray = this.UserArray();
+      char[] revUserInputArray = this.ReverseArray();
+      bool compareArrays = userInputArray.SequenceEqual(revUserInputArray);
+      if (compareArrays == true)
+      {
+        return true;
+      }
+      else 
+      {
+        return false;
+      }
     }
   }
 }
